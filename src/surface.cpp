f9669b642e60ddf7bab064038bfa4c9812d7982f
Lasty <tristan.lasty@gmail.com>
2015-08-22 12:10:23 +0930

    Screwing around with case insensitive file system, because that still exists in 2015

---
+++ src/surface.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Tristan on 22/08/2015.
+//
+
+#include "surface.h"
---
+++ src/surface.h
@@ -0,0 +1,20 @@
+//
+// Created by Tristan on 22/08/2015.
+//
+
+
+#pragma once
+
+
+class surface
+{
+public:
+	surface();
+
+private:
+
+public:
+
+};
+
+
